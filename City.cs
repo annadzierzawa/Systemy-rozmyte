77c8df0d21de5269546b6bf9a5c5b379c373fb95
Anna Dzierżawa <annadzierzawa@gmail.com>
2020-04-04 19:46:42 +0200

    Add project files.

---
+++ City.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Systemy_rozmyte
+{
+    public class City
+    {
+        public string name { get; set; }
+        public double airPollution { get; set; }
+        public double insolation { get; set; }
+        
+        public List<double> results = new List<double>();
+        public List<double> fuzzyRule1 = new List<double>();
+        public List<double> fuzzyRule2 = new List<double>();
+        public List<double> resultOfRules = new List<double>();
+        
+        public void caculateResult() 
+        {
+            results.Add(product(fuzzyRule1[0], fuzzyRule2[0]));
+            results.Add(product(fuzzyRule1[1], fuzzyRule2[1]));
+            results.Add(product(fuzzyRule1[2], fuzzyRule2[2]));
+        }
+        public City(string name, double airPollution, double insolation)
+        {
+            this.name = name;
+            this.airPollution = airPollution;
+            this.insolation = insolation;
+        }
+       double product(double a, double b)
+        {
+            return a * b;
+        }
+        public double makeDecision()
+        {
+            resultOfRules.Add(1);
+            resultOfRules.Add(2);
+            resultOfRules.Add(3);
+            double decision = 0;
+            double tmp = 0;
+            for (int i = 0; i < resultOfRules.Count; i++) 
+            {
+                decision += results[i] * resultOfRules[i];
+             
+            }
+            for(int i=0; i<results.Count; i++)
+            {
+                tmp += results[i];
+            }
+            double result = decision / tmp;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Systemy_rozmyte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<City> cities = new List<City>();
+            cities.Add(new City("Warszawa", 0.3, 0.6));
+            cities.Add(new City("Kraków", 0.1, 1.0));
+            cities.Add(new City("Gdańsk", 0.9, 0.9));
+            cities.Add(new City("Wrocław", 0.7, 0.8));
+            cities.Add(new City("Katowice", 0.1, 0.3));
+            cities.Add(new City("Poznań", 0.6, 0.7));
+            cities.Add(new City("Gliwice", 0.1, 0.3));
+
+            foreach (var city in cities)
+            {
+                city.fuzzyRule1.Add(trapezoidal(0, 0, 0.1, 0.4, city.airPollution));
+                city.fuzzyRule1.Add(triangle(0.1, 0.4, 0.5, city.airPollution));
+                city.fuzzyRule1.Add(trapezoidal(0.4, 0.5, 1, 1, city.airPollution));
+                city.fuzzyRule2.Add(trapezoidal(0, 0, 0.1, 0.7, city.insolation));
+                city.fuzzyRule2.Add(triangle(0.1, 0.7, 0.9, city.insolation));
+                city.fuzzyRule2.Add(trapezoidal(0.7, 0.9, 1, 1, city.insolation));
+                city.caculateResult();
+               Console.WriteLine(city.makeDecision());
+
+            }
+            Console.ReadKey();
+        }
+        static double triangle(double a, double b, double c, double x)
+        {
+            if ((a < x) && (x <= b))
+            {
+                return (x - a) / (b - a);
+            }
+            if ((b < x) && (x < c))
+            {
+                return (c - x) / (c - b);
+            }
+            return 0;
+        }
+        static double trapezoidal(double a, double b, double c, double d, double x)
+        {
+            if (x <= a) { return 0; }
+            if (x > a && x <= b) { return (x - a) / (b - a); }
+            if (x > b && x < c) { return 1; }
+            
+            if (x >= c && x <= d) { 
+                if (c == d) { 
+                    return 1;
+                } 
+                return (d - x) / (d - c); 
+            }
+            return 0;
+        }
+        
+    }
+}
